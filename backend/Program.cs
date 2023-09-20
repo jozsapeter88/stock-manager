8f8f32d45f29b539248642405932bbf432d2aaed
rolandhajos <hajosr7@hotmail.com>
2023-09-20 16:59:25 +0200

    add Dockerfiles and docker-compose

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,7 +9,11 @@ using StockBackend.Service;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+string envType = builder.Environment.IsDevelopment()
+    ? "DefaultConnection"
+    : "DockerCommandsConnectionString";
+var connectionString = builder.Configuration.GetConnectionString(envType) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
--- client/stock-manager/src/Pages/Loading/Loading.jsx
+++
@@ -1,5 +0,0 @@
-import "./Loading.css";
-
-const Loading = () => <div className="Loading" />;
-
-export default Loading;
