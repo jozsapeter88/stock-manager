7bd3980287e3afae472fb8e0fc8d46f8764d7b05
rolandhajos <hajosr7@hotmail.com>
2023-09-24 18:49:40 +0200

    add UserControllerTests

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -16,45 +16,30 @@ public class UserController: ControllerBase
     }
     
     [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody]RegisterModel model)
+    public async Task<ActionResult> Register([FromBody]RegisterModel model)
     {
         Console.WriteLine(model.UserName);
-        if (ModelState.IsValid)
-        {
-            var user = new User { UserName = model.UserName, Email = model.Email, Role = RoleEnum.User};
-            var result = await _userService.RegisterUserAsync(user, model.Password);
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var user = new User { UserName = model.UserName, Email = model.Email, Role = RoleEnum.User};
+        var result = await _userService.RegisterUserAsync(user, model.Password);
 
-            if (result.Succeeded)
-            {
-                return Ok();
-            }
-            return BadRequest(result.Errors);
+        if (result.Succeeded)
+        {
+            return Ok();
         }
-        return BadRequest(ModelState);
+        return BadRequest(result.Errors);
     }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody]LoginModel model)
     {
-        if (ModelState.IsValid)
-        {
-            var result = await _userService.SignInAsync(model.UserName, model.Password, model.RememberMe);
-            
-            if (result.Succeeded)
-            {
-                var user = await _userService.GetUserByName(model.UserName);
-               
-                Console.WriteLine(user.UserName);
-                Console.WriteLine("role: " + user.Role);
-                Console.WriteLine(model.RememberMe);
-                return Ok(user);
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var result = await _userService.SignInAsync(model.UserName, model.Password, model.RememberMe);
 
-            }
-
-            return BadRequest("Login failed.");
-        }
-
-        return BadRequest(ModelState);
+        if (!result.Succeeded) return BadRequest("Login failed.");
+        var user = await _userService.GetUserByName(model.UserName);
+        
+        return Ok(user);
     }
 
     [HttpGet("getUsers")]
