86e2b20d37e7d3bb429b3fe4028578c48718205d
rolandhajos <hajosr7@hotmail.com>
2023-09-26 11:16:48 +0200

    add test to FacilityServiceTests

--- StockBackendTest/Fixtures/ServiceFixture.cs
+++ StockBackendTest/Fixtures/ServiceFixture.cs
@@ -8,83 +8,108 @@ namespace StockBackendTest.Fixtures;
 
 public class ServiceFixture : IDisposable
 {
-   public async Task<(User, Facility, List<Item>)> AddUserAndFacilityToInMemoryDb(DbContextOptions<ApplicationDbContext> options)
-    {
-        await using var context = new ApplicationDbContext(options);
+   public async Task<(List<User>, Facility, List<Item>)> AddUserAndFacilityToInMemoryDb(DbContextOptions<ApplicationDbContext> options)
+ {
+     await using var context = new ApplicationDbContext(options);
 
-        // Declare the variables
-        User user;
-        Facility facility;
-        List<Item> items;
+     // Declare the variables
+     List<User> users;
+     Facility facility;
+     List<Item> items;
 
-        // Check if the database is empty and seed if necessary
-        if (!context.Users.Any())
-        {
-            user = new User
-            {
-                Id = "test-Id",
-                UserName = "testUser",
-                Email = "testUser@example.com",
-                Role = RoleEnum.User
-            };
-            context.Users.Add(user);
-        }
-        else
-        {
-            user = await context.Users.FirstOrDefaultAsync(user => user.Id == "test-Id" );
-        }
+     // Check if the database is empty and seed if necessary
+     if (!context.Users.Any())
+     {
+         users = new List<User>()
+         {
+             new User
+             {
+                 Id = "test-Id",
+                 UserName = "testUser",
+                 Email = "testUser@example.com",
+                 Role = RoleEnum.User
+             },
+             new User()
+             {
+                 Id = "test-Id2",
+                 UserName = "testUserAlreadyHasFacility",
+                 Email = "testUser2@example.com",
+                 Role = RoleEnum.User,
+                 FacilitiesOfUser = new List<Facility>()
+                 {
+                     new Facility()
+                     {
+                         Id = 2,
+                         Name = "TestFacilityUserAlreadyHas",
+                         Sport = SportEnum.Soccer,
+                         CountryCode = "ExampleCode",
+                         PostCode = "ExamplePostCode",
+                         City = "TestCity",
+                         Address = "TestAddress"
+                     }
+                 }
+             }
+         };
+            
+         context.Users.AddRange(users);
+     }
+     else
+     {
+         users = context.Users.ToList();
+     }
 
-        if (!context.Facilities.Any())
-        {
-            facility = new Facility()
-            {
-                Id = 1,
-                Name = "TestFacility",
-                Sport = SportEnum.Soccer,
-                CountryCode = "ExampleCode",
-                PostCode = "ExamplePostCode",
-                City = "TestCity",
-                Address = "TestAddress",
-            };
-            context.Facilities.Add(facility);
-        }
-        else
-        {
-            facility = await context.Facilities.FirstOrDefaultAsync(facility => facility.Id == 1);
-        }
+     if (!context.Facilities.Any())
+     {
+         facility = new Facility()
+         {
+             Id = 1,
+             Name = "TestFacility",
+             Sport = SportEnum.Soccer,
+             CountryCode = "ExampleCode",
+             PostCode = "ExamplePostCode",
+             City = "TestCity",
+             Address = "TestAddress",
+         };
+         context.Facilities.Add(facility);
+     }
+     else
+     {
+         facility = await context.Facilities.FirstOrDefaultAsync(facility => facility.Id == 1);
+     }
 
-        if (!context.Items.Any())
-        {
-            items = new List<Item>()
-            {
-                new Item()
-                {
-                    Id = 1,
-                    Name = "Boxing Gloves",
-                    Sport = SportEnum.CombatSports,
-                    Price = 79.99,
-                    Quantity = 10
-                },
-                new Item()
-                {
-                    Id = 2,
-                    Name = "Mouthguard",
-                    Sport = SportEnum.CombatSports,
-                    Price = 9.99,
-                    Quantity = 20
-                }
-            };
-            context.AddRange(items);
-        }
-        else
-        {
-            items = context.Items.ToList();
-        }
+     if (!context.Items.Any())
+     {
+         items = new List<Item>()
+         {
+             new Item()
+             {
+                 Id = 1,
+                 Name = "Boxing Gloves",
+                 Sport = SportEnum.CombatSports,
+                 Price = 79.99,
+                 Quantity = 10
+             },
+             new Item()
+             {
+                 Id = 2,
+                 Name = "Mouthguard",
+                 Sport = SportEnum.CombatSports,
+                 Price = 9.99,
+                 Quantity = 20
+             }
+         };
+         context.AddRange(items);
+     }
+     else
+     {
+         items = context.Items.ToList();
+     }
 
-        await context.SaveChangesAsync();
+     await context.SaveChangesAsync();
 
-        return (user, facility, items)!;
-    }
+     return (users, facility, items)!;
+ }
+
     public void Dispose()
     {
         
--- StockBackendTest/ServiceTests/FacilityServiceTest.cs
+++ StockBackendTest/ServiceTests/FacilityServiceTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using StockBackend.Areas.Identity.Enums;
+using StockBackend.Models;
 using StockBackend.Models.DBContext;
 using StockBackend.Service;
 using StockBackendTest.Fixtures;
@@ -23,7 +25,7 @@ public class FacilityServiceTest
             .Options;
         //Seed InMemoryDb with a user and a facility
         await _serviceFixture.AddUserAndFacilityToInMemoryDb(options);
-
+       
         await using var context = new ApplicationDbContext(options);
         //Act
         var facilityService = new FacilityService(context);
@@ -36,6 +38,28 @@ public class FacilityServiceTest
         Assert.Equal(1, 
             user!.FacilitiesOfUser!
                 .First(facility => facility.Name == "TestFacility").Id);
+        Assert.Equal(1, user!.FacilitiesOfUser!.Count);
     }
     
+    [Fact]
+    public async Task AddFacilityToUser_ToUserWithFacility()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase($"StockDBForTesting")
+            .Options;
+        //Seed InMemoryDb with a user and a facility and items
+        await _serviceFixture.AddUserAndFacilityToInMemoryDb(options);
+
+        await using var context = new ApplicationDbContext(options);
+        //Act
+        var facilityService = new FacilityService(context);
+        await facilityService.AddFacilityToUser(1, "test-Id2");
+        await context.SaveChangesAsync();
+        //Assert
+        var user = await context.Users
+            .Include(user => user.FacilitiesOfUser)
+            .FirstOrDefaultAsync(u => u.UserName == "testUserAlreadyHasFacility");
+        Assert.Equal(2, user!.FacilitiesOfUser!.Count);
+    }
+
 }
