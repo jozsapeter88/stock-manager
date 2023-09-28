8c1e08dfa885395320fd46f8140dfa690843dc20
jozsapeter88 <jozsapeter88@gmail.com>
2023-09-28 10:09:34 +0200

    add items and cards section to statistics

--- client/src/Pages/Statistics/Statistics.jsx
+++ client/src/Pages/Statistics/Statistics.jsx
@@ -1,50 +1,199 @@
 import React, { useState, useEffect } from "react";
+import { Col, Row, Card, Table, Pagination, PageItem } from "react-bootstrap";
 import TopNavbar from "../Navbar";
+import "./Statistics.css";
 
 const StatisticsPage = () => {
-  const [statisticsData, setStatisticsData] = useState([]);
+  const [facilitiesData, setFacilitiesData] = useState([]);
+  const [itemsData, setItemsData] = useState([]);
+  const [ordersData, setOrdersData] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchFacilities = async () => {
       try {
-        const response = await fetch("http://localhost:5001/api/statistics");
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/facility/facilities`
+        );
+
         if (!response.ok) {
-          throw new Error("Failed to fetch statistics");
+          throw new Error("Failed to fetch facilities");
         }
+
         const data = await response.json();
-        setStatisticsData(data);
+        setFacilitiesData(data);
       } catch (error) {
-        console.error("Error fetching statistics:", error);
+        console.error("Error fetching facilities:", error);
       }
     };
 
-    fetchData();
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/item/getItems`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch items");
+        }
+
+        const data = await response.json();
+        setItemsData(data);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      }
+    };
+
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/order/getAllOrders`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch orders");
+        }
+
+        const data = await response.json();
+        setOrdersData(data);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    };
+
+    fetchFacilities();
+    fetchItems();
+    fetchOrders();
   }, []);
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDisplay = itemsData.slice(startIndex, endIndex);
+
   return (
-    <div>
+    <div className="main-container">
       <TopNavbar />
       <h1>Statistics Page</h1>
-      <table>
+      {/* Cards */}
+
+      <Row>
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Number of Facilities</Card.Title>
+              <Card.Text>{facilitiesData.length}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Number of Orders</Card.Title>
+              <Card.Text>{ordersData.length}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Item Types</Card.Title>
+              <Card.Text>{itemsData.length}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Overall Quantity of Items</Card.Title>
+              <Card.Text>
+                {itemsData.reduce((total, item) => total + item.quantity, 0)}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Average Value of Items</Card.Title>
+              <Card.Text>
+                {itemsData.length > 0
+                  ? (
+                      itemsData.reduce((total, item) => {
+                        if (!isNaN(item.Price) && !isNaN(item.Quantity)) {
+                          return total + item.Price * item.Quantity;
+                        }
+                        return total;
+                      }, 0) /
+                      itemsData.reduce((totalQuantity, item) => {
+                        if (!isNaN(item.Quantity)) {
+                          return totalQuantity + item.Quantity;
+                        }
+                        return totalQuantity;
+                      }, 0)
+                    ).toFixed(2)
+                  : "N/A"}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Overall Value of Items</Card.Title>
+              <Card.Text>
+                {itemsData.reduce((total, item) => total + item.value, 0)}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* Items */}
+      <h2>Items</h2>
+      <Table striped bordered hover>
         <thead>
           <tr>
-            <th>Facility Name</th>
-            <th>Overall Items</th>
-            <th>Overall Value</th>
-            <th>Order Ratio</th>
+            <th>Item Name</th>
+            <th>Quantity</th>
           </tr>
         </thead>
         <tbody>
-          {statisticsData.map((item) => (
-            <tr key={item.facilityName}>
-              <td>{item.facilityName}</td>
-              <td>{item.overallItems}</td>
-              <td>{item.overallValue}</td>
-              <td>{item.orderRatio ? item.orderRatio.toFixed(2) : "N/A"}</td>
+          {itemsToDisplay.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
             </tr>
           ))}
         </tbody>
-      </table>
+      </Table>
+
+      {/* Pagination */}
+      <Pagination size="md">
+        {Array.from({ length: Math.ceil(itemsData.length / itemsPerPage) }).map(
+          (_, index) => (
+            <PageItem
+              key={index}
+              active={index + 1 === currentPage}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </PageItem>
+          )
+        )}
+      </Pagination>
     </div>
   );
 };
