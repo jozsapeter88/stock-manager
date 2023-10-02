8334d61ecd42869a51c863a25bb96146abd6af78
jozsapeter88 <jozsapeter88@gmail.com>
2023-10-02 16:25:58 +0200

    format SupplierPage

--- client/src/Pages/SupplierPage/SupplierPage.jsx
+++ client/src/Pages/SupplierPage/SupplierPage.jsx
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Table, Button, Modal, Form } from "react-bootstrap";
 import TopNavbar from "../Navbar";
-import './SupplierPage.css';
+import "./SupplierPage.css";
 
 function SupplierPage() {
-  const initialSuppliers = [
-    {
-      id: 1,
-      name: "Supplier 1",
-      category: "Category A",
-      location: "Location 1",
-      comment: "Comment 1",
-    },
-    {
-      id: 2,
-      name: "Supplier 2",
-      category: "Category B",
-      location: "Location 2",
-      comment: "Comment 2",
-    },
-  ];
-
-  const [suppliers, setSuppliers] = useState(initialSuppliers);
+  const [suppliers, setSuppliers] = useState([]);
   const [showModal, setShowModal] = useState(false);
-
   const [formData, setFormData] = useState({
     name: "",
     category: "",
@@ -56,102 +38,150 @@ function SupplierPage() {
     createSupplier(newSupplier);
   };
 
-  const createSupplier = (newSupplier) => {
-    console.log("Creating a new supplier:", newSupplier);
+  const fetchAllSuppliers = async () => {
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API_URL + `/supplier/getAllSuppliers`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      const data = await response.json();
+      setSuppliers(data);
+    } catch (error) {
+      console.error("Error fetching suppliers", error);
+    }
   };
+
+  const createSupplier = async (newSupplier) => {
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API_URL + `/supplier/addSupplier`,
+        {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newSupplier),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("New supplier saved:", data);
+
+      setSuppliers([...suppliers, data]);
+
+      setFormData({
+        name: "",
+        category: "",
+        location: "",
+        comment: "",
+      });
+      setShowModal(false);
+    } catch (error) {
+      console.error("Error saving supplier:", error);
+    }
+  };
+  useEffect(() => {
+    fetchAllSuppliers();
+  }, []);
 
   return (
     <>
       <TopNavbar />
-    <div className="main-container">
+      <div className="main-container">
+        <h1>Suppliers</h1>
 
-      <h1>Suppliers</h1>
-
-      {/* Table */}
-      <Table striped bordered hover style={{ outline: "2px solid" }}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Category</th>
-            <th>Location</th>
-            <th>Comment</th>
-          </tr>
-        </thead>
-        <tbody>
-          {suppliers.map((supplier) => (
-            <tr key={supplier.id}>
-              <td>{supplier.name}</td>
-              <td>{supplier.category}</td>
-              <td>{supplier.location}</td>
-              <td>{supplier.comment}</td>
+        {/* Table */}
+        <Table striped bordered hover style={{ outline: "2px solid" }}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Location</th>
+              <th>Comment</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {suppliers.map((supplier) => (
+              <tr key={supplier.id}>
+                <td>{supplier.name}</td>
+                <td>{supplier.category}</td>
+                <td>{supplier.location}</td>
+                <td>{supplier.comment}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
 
-      <Button variant="warning" onClick={() => setShowModal(true)}>
-      Register New Supplier
-      </Button>
+        <Button variant="warning" onClick={() => setShowModal(true)}>
+          Register New Supplier
+        </Button>
 
-      {/* Modal for adding new suppliers */}
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Register New Supplier</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="name">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
+        {/* Modal for adding new suppliers */}
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Register New Supplier</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="category">
-              <Form.Label>Category</Form.Label>
-              <Form.Control
-                type="text"
-                name="category"
-                value={formData.category}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
+              <Form.Group controlId="category">
+                <Form.Label>Category</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="category"
+                  value={formData.category}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="location">
-              <Form.Label>Location</Form.Label>
-              <Form.Control
-                type="text"
-                name="location"
-                value={formData.location}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
+              <Form.Group controlId="location">
+                <Form.Label>Location</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="location"
+                  value={formData.location}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="comment">
-              <Form.Label>Comment</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                name="comment"
-                value={formData.comment}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowModal(false)}>
-            Close
-          </Button>
-          <Button variant="warning" onClick={handleAddSupplier}>
-            Save
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
+              <Form.Group controlId="comment">
+                <Form.Label>Comment</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  name="comment"
+                  value={formData.comment}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowModal(false)}>
+              Close
+            </Button>
+            <Button variant="warning" onClick={handleAddSupplier}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </>
   );
 }
