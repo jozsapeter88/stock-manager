9766e7c197808613f12aed6440b4c745dab09fd1
jozsapeter88 <jozsapeter88@gmail.com>
2023-10-02 09:55:55 +0200

    add Statistics service and controller classes

---
+++ backend/Controllers/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockBackend.Service;
+
+namespace StockBackend.Controllers;
+
+[ApiController]
+[Route("api")]
+public class StatisticsController : ControllerBase
+{
+    private readonly IStatisticsService _statisticsService;
+
+    public StatisticsController(IStatisticsService statisticsService)
+    {
+        _statisticsService = statisticsService;
+    }
+
+    [HttpGet("calculateInventoryItems/{facilityId}")]
+    public IActionResult CalculateInventoryItems(long facilityId)
+    {
+        int result = _statisticsService.CalculateInventoryItems(facilityId);
+        return Ok(result);
+    }
+
+    [HttpGet("calculateInventoryValue/{facilityId}")]
+    public IActionResult CalculateInventoryValue(long facilityId)
+    {
+        double result = _statisticsService.CalculateInventoryValue(facilityId);
+        return Ok(result);
+    }
+
+    [HttpGet("calculateOrdersLinked/{facilityId}")]
+    public IActionResult CalculateOrdersLinked(long facilityId)
+    {
+        int result = _statisticsService.CalculateOrdersLinked(facilityId);
+        return Ok(result);
+    }
+}
