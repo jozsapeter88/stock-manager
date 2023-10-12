d5a4d4c7c9ee66c2bc22acd048206c536743f1c1
jozsapeter88 <jozsapeter88@gmail.com>
2023-10-12 10:44:43 +0200

    delete item Id in DataSeed

--- backend/Models/DBContext/DataSeed.cs
+++ backend/Models/DBContext/DataSeed.cs
@@ -87,35 +87,30 @@ public class DataSeed
             {
                 new Item()
                 {
-                    Id = 1,
                     Name = "Boxing Gloves",
                     Price = 79.99,
                     SuggestedQuantity = 10
                 },
                 new Item()
                 {
-                    Id = 2,
                     Name = "Mouthguard",
                     Price = 9.99,
                     SuggestedQuantity = 10
                 },
                 new Item()
                 {
-                    Id = 3,
                     Name = "Headgear",
                     Price = 49.99,
                     SuggestedQuantity = 5
                 },
                 new Item()
                 {
-                    Id = 4,
                     Name = "Shin Guards",
                     Price = 39.99,
                     SuggestedQuantity = 10
                 },
                 new Item()
                 {
-                    Id = 5,
                     Name = "MMA Shorts",
                     Price = 29.99,
                     SuggestedQuantity = 15
@@ -123,35 +118,30 @@ public class DataSeed
                 // Soccer items
                 new Item()
                 {
-                    Id = 6,
                     Name = "Soccer Ball",
                     Price = 24.99,
                     SuggestedQuantity = 20
                 },
                 new Item()
                 {
-                    Id = 7,
                     Name = "Soccer Shoes",
                     Price = 89.99,
                     SuggestedQuantity = 8
                 },
                 new Item()
                 {
-                    Id = 8,
                     Name = "Soccer Jersey",
                     Price = 49.99,
                     SuggestedQuantity = 12
                 },
                 new Item()
                 {
-                    Id = 9,
                     Name = "Soccer Goal",
                     Price = 149.99,
                     SuggestedQuantity = 5
                 },
                 new Item()
                 {
-                    Id = 10,
                     Name = "Soccer Training Cones",
                     Price = 9.99,
                     SuggestedQuantity = 30
@@ -159,35 +149,30 @@ public class DataSeed
                 // American Football items
                 new Item()
                 {
-                    Id = 11,
                     Name = "Football - Wilson",
                     Price = 29.99,
                     SuggestedQuantity = 15
                 },
                 new Item()
                 {
-                    Id = 12,
                     Name = "Football Helmet",
                     Price = 159.99,
                     SuggestedQuantity = 8
                 },
                 new Item()
                 {
-                    Id = 13,
                     Name = "Football Gloves",
                     Price = 39.99,
                     SuggestedQuantity = 20
                 },
                 new Item()
                 {
-                    Id = 14,
                     Name = "Football Shoulder Pads",
                     Price = 89.99,
                     SuggestedQuantity = 12
                 },
                 new Item()
                 {
-                    Id = 15,
                     Name = "Football Cones",
                     Price = 9.99,
                     SuggestedQuantity = 30
@@ -195,35 +180,30 @@ public class DataSeed
                 // Basketball items
                 new Item()
                 {
-                    Id = 16,
                     Name = "Basketball - Spalding",
                     Price = 24.99,
                     SuggestedQuantity = 20
                 },
                 new Item()
                 {
-                    Id = 17,
                     Name = "Basketball Shoes",
                     Price = 99.99,
                     SuggestedQuantity = 8
                 },
                 new Item()
                 {
-                    Id = 18,
                     Name = "Basketball Jersey",
                     Price = 49.99,
                     SuggestedQuantity = 12
                 },
                 new Item()
                 {
-                    Id = 19,
                     Name = "Basketball Hoop",
                     Price = 199.99,
                     SuggestedQuantity = 5
                 },
                 new Item()
                 {
-                    Id = 20,
                     Name = "Basketball Training Cones",
                     Price = 9.99,
                     SuggestedQuantity = 30
@@ -231,35 +211,30 @@ public class DataSeed
                 // Tennis items
                 new Item()
                 {
-                    Id = 21,
                     Name = "Tennis Racket - Wilson",
                     Price = 59.99,
                     SuggestedQuantity = 10
                 },
                 new Item()
                 {
-                    Id = 22,
                     Name = "Tennis Balls",
                     Price = 14.99,
                     SuggestedQuantity = 50
                 },
                 new Item()
                 {
-                    Id = 23,
                     Name = "Tennis Shoes",
                     Price = 79.99,
                     SuggestedQuantity = 8
                 },
                 new Item()
                 {
-                    Id = 24,
                     Name = "Tennis Bag",
                     Price = 39.99,
                     SuggestedQuantity = 15
                 },
                 new Item()
                 {
-                    Id = 25,
                     Name = "Tennis Training Cones",
                     Price = 9.99,
                     SuggestedQuantity = 30
@@ -267,35 +242,30 @@ public class DataSeed
                 // Swimming items
                 new Item()
                 {
-                    Id = 26,
                     Name = "Swimming Goggles",
                     Price = 14.99,
                     SuggestedQuantity = 20
                 },
                 new Item()
                 {
-                    Id = 27,
                     Name = "Swimming Cap",
                     Price = 9.99,
                     SuggestedQuantity = 30
                 },
                 new Item()
                 {
-                    Id = 28,
                     Name = "Swimming Fins",
                     Price = 29.99,
                     SuggestedQuantity = 15
                 },
                 new Item()
                 {
-                    Id = 29,
                     Name = "Swimming Kickboard",
                     Price = 19.99,
                     SuggestedQuantity = 20
                 },
                 new Item()
                 {
-                    Id = 30,
                     Name = "Swimming Training Cones",
                     Price = 9.99,
                     SuggestedQuantity = 30
---
+++ backend/Models/DTO/ItemDto.cs
@@ -0,0 +1,11 @@
+﻿using StockBackend.Areas.Identity.Enums;
+
+namespace StockBackend.Models.DTO;
+
+public class ItemDto
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+    public int SuggestedQuantity { get; set; }
+}
