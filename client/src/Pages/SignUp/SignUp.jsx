1417ab8439647ef5c434a8c127be53bac1d5cc9c
jozsapeter88 <jozsapeter88@gmail.com>
2023-10-18 22:20:43 +0200

    change landing page font and alignment

--- client/src/Pages/SignUp/SignUp.jsx
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -1,13 +1,22 @@
 import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
-import "./SignUp.css"
+import "./SignUp.css";
 
-export default function SignUp({onSubmit, showMsg, successfulReg}) {
+export default function SignUp({ onSubmit, showMsg, successfulReg }) {
   return (
     <div>
       <Container>
         <Row className="vh-100 d-flex justify-content-center align-items-center">
-        <Col md={4} lg={6} xs={12}>
-            <img src="https://i.imgur.com/S0upujn.png" alt="img" className="img-fluid"  style={{ marginLeft: '-10rem'}}/>
+          <Col md={4} lg={6} xs={12}>
+            <img
+              src={process.env.PUBLIC_URL + "/newlogo_transparent.png"}
+              alt="img"
+              className="img-fluid"
+              style={{
+                marginLeft: "",
+                maxWidth: "600px",
+                maxHeight: "600px",
+              }}
+            />
           </Col>
           <Col md={8} lg={6} xs={12}>
             <div className="border border-0 border-primary"></div>
@@ -15,20 +24,29 @@ export default function SignUp({onSubmit, showMsg, successfulReg}) {
               <Card.Body>
                 <div className="mb-3 mt-md-4">
                   <h2 className="fw-bold mb-2">Sign up</h2>
-                  <p className=" mb-5" ></p>
+                  <p className=" mb-5"></p>
                   <div className="mb-3">
                     <Form onSubmit={onSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label className="text-center" >
-                          Email
-                        </Form.Label>
-                        <Form.Control type="input" placeholder="Enter email" style={{borderColor: showMsg ? '' : 'red'}}/>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">Email</Form.Label>
+                        <Form.Control
+                          type="input"
+                          placeholder="Enter email"
+                          style={{ borderColor: showMsg ? "" : "red" }}
+                        />
                       </Form.Group>
-                      <Form.Group className="mb-3" controlId="formBasicUsername">
-                        <Form.Label className="text-center" >
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicUsername"
+                      >
+                        <Form.Label className="text-center">
                           Username
                         </Form.Label>
-                        <Form.Control type="input" placeholder="Enter username" style={{borderColor: showMsg ? '' : 'red'}}/>
+                        <Form.Control
+                          type="input"
+                          placeholder="Enter username"
+                          style={{ borderColor: showMsg ? "" : "red" }}
+                        />
                       </Form.Group>
 
                       <Form.Group
@@ -41,19 +59,26 @@ export default function SignUp({onSubmit, showMsg, successfulReg}) {
                       <Form.Group
                         className="mb-3"
                         controlId="formBasicCheckbox"
-                      >
-                      </Form.Group>
+                      ></Form.Group>
                       <div className="d-grid">
                         <Button variant="warning" type="submit">
-                            Sign Up
+                          Sign Up
                         </Button>
                       </div>
                     </Form>
-                    <div className="mt-3" >
-                    <p className="mb-0  text-center" hidden={showMsg} id="not-valid">
+                    <div className="mt-3">
+                      <p
+                        className="mb-0  text-center"
+                        hidden={showMsg}
+                        id="not-valid"
+                      >
                         Username is already taken.
                       </p>
-                    <p className="mb-0  text-center" hidden={!successfulReg} id="valid">
+                      <p
+                        className="mb-0  text-center"
+                        hidden={!successfulReg}
+                        id="valid"
+                      >
                         Successful registration!
                       </p>
                     </div>
